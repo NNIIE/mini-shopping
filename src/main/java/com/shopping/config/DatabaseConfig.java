9b306facb1b079dec72c83372f779b319bbf6b07
nniie <greg0917@naver.com>
2025-04-22 22:28:16 +0900

    feat: 초기 설정

--- src/main/java/com/shopping/config/DatabaseConfig.java
+++ src/main/java/com/shopping/config/DatabaseConfig.java
@@ -1,16 +1,21 @@
 package com.shopping.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaAuditing
+@EnableTransactionManagement
 public class DatabaseConfig {
 
     @Bean
@@ -18,6 +23,12 @@ public class DatabaseConfig {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 
 }
